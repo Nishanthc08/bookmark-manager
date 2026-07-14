8761795a15a28fd9d5eb18fede215acc2a1c0eb8
nishanthc <nishanthc@cdac.in>
2026-07-14 11:50:06 +0530

    restructure: move bm script to repo root, clean up build artifacts from tracking

--- build.sh
+++ build.sh
@@ -15,15 +15,22 @@ else
   rm -rf "$DEST" "../$DEB" "../bm_1.0.0"*".buildinfo" "../bm_1.0.0"*".changes"
 
   install -d "$DEST/DEBIAN" "$DEST/usr/bin" "$DEST/usr/share/man/man1"
-  install -d "$DEST/usr/share/bash-completion/completions" "$DEST/usr/share/doc/bm"
+  install -d "$DEST/usr/share/bash-completion/completions" "$DEST/usr/share/doc/bm" "$DEST/usr/share/bm"
+  install -d "$DEST/etc/profile.d"
 
   install -m 755 bm "$DEST/usr/bin/"
   install -m 644 bm.1 "$DEST/usr/share/man/man1/"
   gzip -9n "$DEST/usr/share/man/man1/bm.1"
   install -m 644 bash-completion/bm "$DEST/usr/share/bash-completion/completions/"
+  install -m 644 bm.sh "$DEST/usr/share/bm/"
+  install -m 644 etc/profile.d/bm.sh "$DEST/etc/profile.d/"
   install -m 644 debian/changelog "$DEST/usr/share/doc/bm/changelog"
   install -m 644 debian/copyright "$DEST/usr/share/doc/bm/"
   gzip -9n "$DEST/usr/share/doc/bm/changelog"
+
+  cp debian/postinst "$DEST/DEBIAN/"
+  cp debian/prerm "$DEST/DEBIAN/"
+  cp debian/conffiles "$DEST/DEBIAN/"
 
   dpkg-gencontrol -P"$DEST"
   dpkg-deb --root-owner-group --build "$DEST" ..
